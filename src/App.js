768460ae5570b8a764717e23d819ae5f6bd272c4
EinsVision <einsvision@gmail.com>
2022-06-06 15:51:51 +0200

    Install Material UI

--- src/App.js
+++ src/App.js
@@ -8,17 +8,23 @@ function App() {
   const [input, setInput] = useState('');
 
   const addTodo = (event) =>{
+    event.preventDefault();
     setTodos([...todos, input]); //input: here is new todo.
+    setInput('');
   }
 
   return (
     <div className="App">
       <div className='App__container'>
         <h1>Hello React 🎨</h1>
-        <input value={input} onChange={
-          event => setInput(event.target.value)
-        }/>
-        <button onClick={addTodo}>Add Todo</button>
+
+        <form>
+          <input value={input} onChange={
+            event => setInput(event.target.value)
+          }/>
+          <button type='submit' onClick={addTodo}>Add Todo</button>
+        </form>
+        
         <ul>
           {todos.map(todo => (
             <li>{todo}</li>
