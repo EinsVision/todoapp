b846c845b332f3b1b57235418c46aa667ca0f4e0
EinsVision <einsvision@gmail.com>
2022-06-06 15:42:05 +0200

    addTodo function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [todos, setTodos] = useState(['Cooking Pasta 🍝', 'Take a Walk 🏃‍♂️', 'Fitting Glasses 🕶️ 👓']);
+  // todos: short term memory
+  const [input, setInput] = useState('');
+
+  const addTodo = (event) =>{
+    setTodos([...todos, input]); //input: here is new todo.
+  }
+
   return (
     <div className="App">
-      <h1>
-        Hello React 🎨
-      </h1>
+      <div className='App__container'>
+        <h1>Hello React 🎨</h1>
+        <input value={input} onChange={
+          event => setInput(event.target.value)
+        }/>
+        <button onClick={addTodo}>Add Todo</button>
+        <ul>
+          {todos.map(todo => (
+            <li>{todo}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
